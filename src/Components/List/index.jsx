0a0da2c49705ce62af60f5f86850f3e6e2f80c5b
Nate Brown <nathan.d.brown@gmail.com>
2023-05-27 21:23:53 -0700

    finishing 33/34

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -25,24 +25,3 @@ function List (props) {
 }
 
 export default List;
-
-
-
-// {paginate(list, currentPosition, settings.itemsToDisplay).map(item => (
-//   <div key={item.id}>
-//     <p>{item.text}</p>
-//     <p><small>Assigned to: {item.assignee}</small></p>
-//     <p><small>Difficulty: {item.difficulty}</small></p>
-//     <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-//     <hr />
-//   </div>
-// ))}
-
-// <Pagination
-//   onChange={(page) => setCurrentPosition(page)}
-//   total={calculateTotal()}
-//   color="indigo"
-//   size="lg"
-//   radius="md"
-//   initialPage={currentPosition} />
-// </>
